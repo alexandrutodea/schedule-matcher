547103d1342118a5b7f5007b8f3f511d0d2c4ed8
Alexandru Todea <alexandru.todea01@e-uvt.ro>
2022-03-25 16:38:59 +0200

    Removed module-info.java

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.example.schedulematcher {
-    requires javafx.controls;
-    requires javafx.fxml;
-    exports com.endava.tmd.soj.schedulematcher.ui;
-    exports com.endava.tmd.soj.schedulematcher.model;
-}
