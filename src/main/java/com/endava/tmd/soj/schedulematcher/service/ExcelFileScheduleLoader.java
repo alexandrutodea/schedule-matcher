ba706687ef9edc8f28463b074ba7e05dcdefbf27
Adrian Meran <adrian.meran.99@gmail.com>
2022-03-26 19:57:09 +0200

    to be fixed

--- src/main/java/com/endava/tmd/soj/schedulematcher/service/ExcelFileScheduleLoader.java
+++ src/main/java/com/endava/tmd/soj/schedulematcher/service/ExcelFileScheduleLoader.java
@@ -1,73 +1,221 @@
 package com.endava.tmd.soj.schedulematcher.service;
 
 import com.endava.tmd.soj.schedulematcher.exception.InvalidExcelFileException;
+import com.endava.tmd.soj.schedulematcher.model.Day;
+import com.endava.tmd.soj.schedulematcher.model.IntervalColor;
 import com.endava.tmd.soj.schedulematcher.model.Schedule;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import com.endava.tmd.soj.schedulematcher.model.TimeInterval;
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.*;
 
 
 public class ExcelFileScheduleLoader implements ScheduleLoader{
+
+    static final String EXCEPTION_MESSAGE = "***********";
+
+    Day[] days = Day.values();
+    String[] intervalColors = {"#FF0000", "#FFFF00", "#7CFC00", "#FFFFFF"};
+
 
     @Override
     public Schedule loadSchedule(InputStream inputStream) {
         //throw new UnsupportedOperationException();
 
-        try(Workbook workbook = WorkbookFactory.create(inputStream)) {
-            if (!validateHeaders(workbook))
-                throw new InvalidExcelFileException("Invalid Excel file format");
+        try(XSSFWorkbook workbook = (XSSFWorkbook) WorkbookFactory.create(inputStream)) {
+            Schedule schedule = new Schedule();
+//            if (validateHeaders(workbook))
+                buildSchedule(workbook, schedule);
+            
+            return schedule;
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-        return new Schedule();
+        throw new InvalidExcelFileException("headers not valid");
+
     }
 
 
-
+    //TODO: to fix validation;  not working at all for non color tests
     public boolean validateHeaders(Workbook workbook) throws IOException {
         Sheet sheet = workbook.getSheetAt(0);
-        Row row = sheet.getRow(0);
 
-        Workbook templateWorkbook = new XSSFWorkbook(new FileInputStream("src/main/resources/com/endava/tmd/soj/scheduleTemplate.xlsx"));
+        XSSFWorkbook templateWorkbook = new XSSFWorkbook(new FileInputStream("src/main/resources/com/endava/tmd/soj/scheduleTemplate.xlsx"));
         Sheet templateSheet = templateWorkbook.getSheetAt(0);
-        Row templateRow = templateSheet.getRow(0);
 
         Cell cell;
         Cell templateCell;
+        boolean equals;
 
+        //TODO: update Excel template
+        Row row = sheet.getRow(0);
+        Row templateRow = templateSheet.getRow(0);
 
         for (int cellNumber = 1; cellNumber < 8; cellNumber++) {
             cell = row.getCell(cellNumber);
             templateCell = templateRow.getCell(cellNumber);
 
-            boolean equals = cell.getRichStringCellValue().getString()
+            equals = cell.getRichStringCellValue().getString()
                     .equals(templateCell.getRichStringCellValue().getString());
 
             if (!equals)
-                return false;
+                throw new InvalidExcelFileException("Invalid template weekdays");
         }
 
-        for (int rowNumber = 1; rowNumber < 12; rowNumber++) {
+        //TODO: change to rownumber < 12 when Excel files get fixed
+        for (int rowNumber = 1; rowNumber < 11; rowNumber++) {
             row = sheet.getRow(rowNumber);
             templateRow = templateSheet.getRow(rowNumber);
 
-            boolean equals = row.getCell(0).getRichStringCellValue().getString()
+            equals = row.getCell(0).getRichStringCellValue().getString()
                     .equals(templateRow.getCell(0).getRichStringCellValue().getString());
 
             if (!equals)
-                return false;
+                throw new InvalidExcelFileException("Invalid template intervals");
+
+            //TODO: maybe update the tests
         }
 
         return true;
     }
 
+
+
+    public void buildSchedule(XSSFWorkbook workbook, Schedule schedule) {
+
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        boolean fileContainsColors = false;
+        boolean fileContainsXs = false;
+
+        //TODO: fix Excel File template
+        for (int i = 1; i <= 7; i++) {
+            Day day = days[i - 1];
+
+            for (int j = 1; j <= 10; j++) {
+                XSSFRow row = sheet.getRow(j);
+                XSSFCell cell = row.getCell(i);
+
+//                boolean cellIsEmpty = false; //= cell.getRichStringCellValue().getString().isEmpty();
+                boolean cellContainsX = false; //cell.getRichStringCellValue().getString().equals("x");
+
+                //TODO: fix hasDefaultColor
+                boolean cellHasDefaultColor = false; //cell.getCellStyle().getFillBackgroundColor() == 0;
+
+                if (cell == null || cell.getCellType() == CellType.BLANK) {
+                    continue;
+                } else {
+                    if (cell.getRichStringCellValue().getString().equals("x"))
+                        cellContainsX = true;
+                    if (cell.getCellStyle().getFillBackgroundColor() == IndexedColors.AUTOMATIC.getIndex())
+                        cellHasDefaultColor = true;
+                }
+
+
+                //TODO: to fix ColorInput files - empty Schedule returned; works for X-marked files
+
+                if (cellContainsX && cellHasDefaultColor) {
+                    fileContainsXs = true;
+
+                    int[] interval = extractInterval(cell, sheet);
+                    schedule.addBusyTimeInterval(day, new TimeInterval(interval[0], interval[1]));
+                }
+                else {//if (!cellContainsX && !cellHasDefaultColor && cellHasAvailabilityColor(cell)) {
+                    fileContainsColors = true;
+
+                    int[] interval = extractInterval(cell, sheet);
+                    schedule.addBusyTimeInterval(day, new TimeInterval(interval[0], interval[1], getAvailabilityColor(cell)));
+                }
+//                else throw new InvalidExcelFileException("invalid input - Xs/Colors");
+
+
+                if (fileContainsColors && fileContainsXs)
+                    throw new InvalidExcelFileException("file contains both Xs and Colors");
+            }
+
+        }
+
+
+    }
+
+
+    //TODO: check getColor method
+
+    public IntervalColor getAvailabilityColor(XSSFCell cell) {
+        XSSFColor cellColor = cell.getCellStyle().getFillBackgroundColorColor();
+
+        //TODO: rgb vs argb
+        for (IntervalColor color : IntervalColor.values()) {
+            if (cellColor.getARGBHex().equals(color.getCode()))
+                return color;
+        }
+
+
+        throw new InvalidExcelFileException("A cell may only be colored in either white, red, green or yellow");
+    }
+//
+//
+//    //TODO: dupe
+//    public byte[] getSpecificForegroundColor(IntervalColor intervalColor) {
+//        byte[] rgb = new byte[3];
+//        rgb[0] = Byte.valueOf(intervalColor.getCode().substring(1, 3), 16);
+//        rgb[1] = Byte.valueOf(intervalColor.getCode().substring(3, 5), 16);
+//        rgb[2] = Byte.valueOf(intervalColor.getCode().substring(5, 7), 16);
+//
+//        return rgb;
+//    }
+
+
+    public boolean cellHasAvailabilityColor(Cell cell) {
+        if (cell == null || cell.getCellType() == CellType.BLANK)
+            return true;
+
+        String colorHex = (( XSSFColor) cell.getCellStyle().getFillBackgroundColorColor()).getARGBHex();
+
+        for (String color : intervalColors) {
+            if (colorHex.equals(color))
+                return true;
+        }
+
+        return false;
+    }
+
+
+    public int[] extractInterval(Cell cell, Sheet sheet) {
+
+        if (cell != null) {
+            Row row = sheet.getRow(cell.getRowIndex());
+
+            Cell intervalCell = row.getCell(0);
+
+            String[] intervals = intervalCell.getRichStringCellValue().getString().split(" - ");
+
+            String[] tempIntervals;
+            int[] finalIntervals = new int[2];
+
+            tempIntervals = intervals[0].split(":", 2);
+            finalIntervals[0] = Integer.parseInt(tempIntervals[0]);
+
+            tempIntervals = intervals[1].split(":", 2);
+            finalIntervals[1] = Integer.parseInt(tempIntervals[0]);
+
+            return finalIntervals;
+
+        } else
+            throw new InvalidExcelFileException("invalid interval format");
+
+
+    }
 
 }
--- src/main/java/com/endava/tmd/soj/schedulematcher/service/ExcelFileScheduleWriter.java
+++ src/main/java/com/endava/tmd/soj/schedulematcher/service/ExcelFileScheduleWriter.java
@@ -1,12 +1,104 @@
 package com.endava.tmd.soj.schedulematcher.service;
 
+import com.endava.tmd.soj.schedulematcher.model.Day;
+import com.endava.tmd.soj.schedulematcher.model.IntervalColor;
 import com.endava.tmd.soj.schedulematcher.model.Schedule;
+import com.endava.tmd.soj.schedulematcher.model.TimeInterval;
+import org.apache.poi.ss.formula.functions.T;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
+import java.util.Optional;
 
 public class ExcelFileScheduleWriter implements ScheduleWriter {
+
+    Day[] days = Day.values();
+
     @Override
     public void writeSchedule(OutputStream outputStream, Schedule schedule) {
-        throw new UnsupportedOperationException();
+        XSSFWorkbook workbook;
+
+        try {
+            workbook = new XSSFWorkbook(new FileInputStream("src/main/resources/com/endava/tmd/soj/scheduleTemplate.xlsx"));
+
+            updateWorkbook(workbook, schedule);
+
+            workbook.write(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //TODO: look into XSSFSheet, XSSFRow, etc
+    public void updateWorkbook(XSSFWorkbook workbook, Schedule schedule) {
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (int i = 1; i <= 7; i++) {
+            Day day = days[i - 1];
+
+            if (schedule.getBusyTimeIntervals(day).isEmpty())
+                continue;
+
+            List<TimeInterval> busyTimeIntervals = schedule.getBusyTimeIntervals(day).get();
+
+            for (int j = 1; j <= 10; j++) {
+                Cell cell = sheet.getRow(j).getCell(i);
+
+                String interval = sheet.getRow(j).getCell(0).getRichStringCellValue().getString();
+
+                XSSFCellStyle style = workbook.createCellStyle();
+                TimeInterval timeInterval = constructTimeInterval(interval);
+                //TODO: research using custom colors (from IntervalColor)
+                if (busyTimeIntervals.contains(timeInterval)) {
+                    style.setFillForegroundColor(new XSSFColor(getSpecificForegroundColor(timeInterval.getIntervalColor())));
+                }
+                else {
+                    style.setFillForegroundColor(new XSSFColor(getSpecificForegroundColor(IntervalColor.GREEN)));
+                }
+
+                cell.setCellStyle(style);
+
+            }
+        }
+
+
+    }
+
+
+    public byte[] getSpecificForegroundColor(IntervalColor intervalColor) {
+        byte[] rgb = new byte[3];
+        rgb[0] = Byte.valueOf(intervalColor.getCode().substring(1, 3), 16);
+        rgb[1] = Byte.valueOf(intervalColor.getCode().substring(3, 5), 16);
+        rgb[2] = Byte.valueOf(intervalColor.getCode().substring(5, 7), 16);
+
+        return rgb;
+    }
+
+//    public String constructStringInterval(TimeInterval timeInterval) {
+//        return timeInterval.getStart() + ":00 - " + timeInterval.getEnd() + ":00";
+//    }
+
+
+    //TODO: check duplicated code
+    public TimeInterval constructTimeInterval(String timeInterval) {
+        String[] intervals = timeInterval.split(" - ");
+
+        String[] tempIntervals;
+        int[] finalIntervals = new int[2];
+
+        tempIntervals = intervals[0].split(":", 2);
+        finalIntervals[0] = Integer.parseInt(tempIntervals[0]);
+
+        tempIntervals = intervals[1].split(":", 2);
+        finalIntervals[1] = Integer.parseInt(tempIntervals[0]);
+
+        return new TimeInterval(finalIntervals[0], finalIntervals[1]);
     }
 }
